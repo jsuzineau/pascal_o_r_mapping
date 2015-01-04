563e32ecb19e4cbd86dc1a67ce6db8caa5332820
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2015-01-04 15:33:04 +0100

    Mise au point sous Windows 64 bits

---
+++ pascal_o_r_mapping/4_Components/od_delphireportengine_components_windows.pas
@@ -0,0 +1,23 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit OD_DelphiReportEngine_Components_windows;
+
+interface
+
+uses
+  ucBatproVerifieur, ucbvCustomConnection, ucbvQuery_Datasource, 
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('ucbvCustomConnection', @ucbvCustomConnection.Register);
+  RegisterUnit('ucbvQuery_Datasource', @ucbvQuery_Datasource.Register);
+end;
+
+initialization
+  RegisterPackage('OD_DelphiReportEngine_Components_windows', @Register);
+end.
