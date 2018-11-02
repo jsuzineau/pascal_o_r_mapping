2f7d0f262704ca2022203cd8a6f0b551a105f100
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2018-11-02 22:51:05 +0100

    test

--- tools/test_gICAPI/angular-test-gICAPI/src/app/app.component.ts
+++ tools/test_gICAPI/angular-test-gICAPI/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import {} from  'jquery';
-import {Metro} from  'metro4-dist';
 import { TgICAPI} from './01_service/gICAPI.service';
 import {Element, Element_Vide} from './01_service/element/element'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None,
-  styleUrls: ['./app.component.css'],
+  styleUrls: 
+    [
+      '../../node_modules/metro4-dist/css/metro-all.css',      
+      './app.component.css'
+    ],
   providers: [TgICAPI],
 })
 export class AppComponent 
