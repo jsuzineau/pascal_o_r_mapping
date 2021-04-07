2126b44443c4c3380d1019c1905dbbbbaea305c5
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2021-04-07 03:52:23 +0200

    Mise au point Quasi_prime

--- tools/Quasi_prime/ufQuasi_prime.pas
+++ tools/Quasi_prime/ufQuasi_prime.pas
@@ -5,17 +5,24 @@ unit ufQuasi_prime;
 interface
 
 uses
+    uuStrings, uReels,
  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
- Spin,math;
+ Spin, ShellCtrls,math;
 
 type
 
  { TfQuasi_prime }
 
  TfQuasi_prime = class(TForm)
+  bBatch: TButton;
+  Label1: TLabel;
+  Label2: TLabel;
+  lP1: TLabel;
+  lP2: TLabel;
   m: TMemo;
   Panel1: TPanel;
   spe: TSpinEdit;
+  procedure bBatchClick(Sender: TObject);
   procedure FormCreate(Sender: TObject);
   procedure speChange(Sender: TObject);
  private
@@ -119,12 +126,114 @@ begin
      Result:= Triangle_a_from_s( s)/3;
 end;
 
+function Intersection_r_from_i( _i: Integer):ValReal;
+var
+   Carre   : TCarre;
+   Triangle: TTriangle;
+   x, y: ValReal;
+begin
+     Carre   := TCarre   .Create( _i);
+     Triangle:= TTriangle.Create( _i);
+     try
+        x:= TAffine.Intersection_x( Triangle, Carre);
+        y:= Triangle.y( x);
+        Result:= sqrt( sqr(x) +sqr(y) )/1.0524;
+     finally
+            FreeAndNil(Carre   );
+            FreeAndNil(Triangle);
+            end;
+end;
+
+type
+
+  { TCalcul }
+
+  TCalcul
+  =
+   object
+     i1, i2, i: Integer;
+     Intersection_r,
+     Mean_Circle_s,
+     Mean,
+     Ecart_s,
+     Ecart,
+     P1,
+     P2: ValReal;
+
+     Erreur: Boolean;
+
+     Intersection_r_,
+     Mean_,
+     Mean_Circle_s_,
+     Ecart_s_,
+     Ecart_ :ValReal;
+
+     procedure Decompose( _i1, _i2: Integer);
+     function Log:String;
+   end;
+
+{ TCalcul }
+
+procedure TCalcul.Decompose( _i1, _i2: Integer);
+begin
+     i1:= _i1;
+     i2:= _i2;
+     i:= i1*i2;
+
+     Intersection_r:= Intersection_r_from_i( i);
+     Mean_Circle_s:=PI*sqr(Intersection_r);
+     Mean:= round(sqrt(Mean_Circle_s)); //Mean = Intersection_r*sqrt(PI) ?
+     Mean_Circle_s:=Mean**2;
+
+     Ecart_s:= Abs( Mean_Circle_s - i);
+
+     Ecart:=ifthen( Ecart_s < 0.01, 0, sqrt( Ecart_s));
+
+     P1:= round(Mean-Ecart);
+     P2:= round(Mean+Ecart);
+
+     Erreur
+     :=
+       not
+          (
+            ((P1=i1)and(P2=i2))
+          or((P1=i2)and(P2=i1))
+          );
+
+     Mean_:= (i1+i2)/2;
+     Mean_Circle_s_:= Mean_**2;
+
+     Ecart_:= (i2-i1)/2;
+     Ecart_s_:= Ecart_**2;
+
+     Intersection_r_:= sqrt(Mean_Circle_s_/PI);
+end;
+
+function TCalcul.Log: String;
+begin
+     Result:= '';
+     Formate_Liste( Result, #13#10, Format('%d * %d = %d', [i1, i2, i]));
+     Formate_Liste( Result, #13#10, Format('Intersection_r: %f attendu %f,  %f%%', [Intersection_r, Intersection_r_, (Intersection_r/Intersection_r_)*100]));
+     Formate_Liste( Result, #13#10, Format('Mean_Circle_s : %f attendu %f', [Mean_Circle_s, Mean_Circle_s_]));
+     Formate_Liste( Result, #13#10, Format('Mean          : %f attendu %f', [Mean, Mean_]));
+     Formate_Liste( Result, #13#10, Format('Ecart_s       : %f attendu %f', [Ecart_s, Ecart_s_]));
+     Formate_Liste( Result, #13#10, Format('Ecart         : %f attendu %f', [Ecart, Ecart_]));
+     Formate_Liste( Result, #13#10, Format('P1            : %f ', [P1]));
+     Formate_Liste( Result, #13#10, Format('P2            : %f ', [P2]));
+end;
+
+function Decompose( _i1, _i2: Integer):TCalcul;
+begin
+     Result.Decompose( _i1, _i2);
+end;
+
+
 { TfQuasi_prime }
 
 procedure TfQuasi_prime.FormCreate(Sender: TObject);
 begin
      m.Clear;
-     Calcule;
+     //Calcule;
 end;
 
 procedure TfQuasi_prime.speChange(Sender: TObject);
@@ -135,48 +244,151 @@ end;
 procedure TfQuasi_prime.Calcule;
 var
    s: Integer;
-   Carre: TCarre;
-   Triangle: TTriangle;
-   Intersection_x,
-   Intersection_y,
-   Intersection_r,
-   Blue_Circle_s,
-   Mean,
-   Ecart_s,
-   Ecart,
+   s_racine:Integer;
+   s_racine_delta: Integer;
+   is_square: Boolean;
+
    P1, P2: ValReal;
-   procedure p( _s: String; _d: ValReal);
+   Feedback_s:Integer;
+   procedure p( _s: String; _d: ValReal; _l: TLabel = nil);
+   var
+      sD: String;
    begin
-        m.Lines.Add( _s+':'+FloatToStr( _d));
+        sD:= FloatToStr( _d);
+        m.Lines.Add( _s+':'+sD);
+        with m.VertScrollBar do Position:= Range;
+        if nil = _l then exit;
+        _l.Caption:= sD;
+   end;
+   procedure l( _l: TLabel; _d: ValReal);
+   var
+      sD: String;
+   begin
+        sD:= FloatToStr( _d);
+        _l.Caption:= sD;
+   end;
+   procedure Traite_Carre;
+   begin
+        P1:= s_racine;
+        P2:= P1;
+        Feedback_s := Trunc(P1*P2);
+
+        p('P1=P2=',P1);
+        p('Feedback_s' ,Feedback_s );
+   end;
+   procedure Cas_General;
+   var
+      Intersection_x,
+      Intersection_y,
+      Intersection_r,
+      Blue_Circle_s,
+      Blue_Circle_s_from_Mean,
+      Mean,
+      Ecart_s,
+      Ecart: ValReal;
+      Feedback_s1:Integer;
+      Feedback_s2:Integer;
+      procedure Calcule_intersection;
+      var
+         Carre   : TCarre;
+         Triangle: TTriangle;
+      begin
+           Carre   := TCarre   .Create( s);
+           Triangle:= TTriangle.Create( s);
+           try
+              Intersection_x:= TAffine.Intersection_x( Triangle, Carre);
+              Intersection_y:= Triangle.y( Intersection_x);
+           finally
+                  FreeAndNil(Carre   );
+                  FreeAndNil(Triangle);
+                  end;
+      end;
+   begin
+        Calcule_intersection;
+
+        Intersection_r:= round(sqrt( sqr(Intersection_x) +sqr(Intersection_y) ));
+        Blue_Circle_s:=PI*sqr(Intersection_r);
+        Mean:= round(sqrt(Blue_Circle_s));
+        Blue_Circle_s_from_Mean:=sqr(Mean);
+        Ecart_s:= Blue_Circle_s_from_Mean - s;
+        Ecart:=round(sqrt( Ecart_s));
+
+        P1:= Mean-Ecart;
+        P2:= Mean+Ecart;
+        Feedback_s := Trunc(P1*P2);
+
+        p('Blue_Circle_s', Blue_Circle_s);
+        p('Blue_Circle_s_from_Mean', Blue_Circle_s_from_Mean);
+        p('Mean', Mean);
+        p('Ecart', Ecart);
+        p('P1',P1);
+        p('P2',P2);
+        p('Feedback_s' ,Feedback_s );
+        if Feedback_s <> s
+        then
+            begin
+            Feedback_s1:= Trunc((P1+1)* P2   );
+            Feedback_s2:= Trunc( P1   *(P2+1));
+            p('Feedback_s1',Feedback_s1);
+            p('Feedback_s2',Feedback_s2);
+            end;
    end;
 begin
      s:= spe.Value;
      m.Lines.Add('');
      p('s', s);
-     Carre:= TCarre.Create( s);
-     Triangle:= TTriangle.Create(s);
 
-     Intersection_x:= TAffine.Intersection_x( Triangle, Carre);
-     Intersection_y:= Triangle.y( Intersection_x);
-     Intersection_r:= round(sqrt( sqr(Intersection_x) +sqr(Intersection_y) ));
-     Blue_Circle_s:=PI*sqr(Intersection_r);
-     p('Blue_Circle_s', Blue_Circle_s);
-     Mean:= round(sqrt(Blue_Circle_s));
-     Blue_Circle_s:=sqr(Mean);
-     Ecart_s:= Blue_Circle_s - s;
-     Ecart:=round(sqrt( Ecart_s));
+     s_racine:= Trunc(sqrt(s));
+     s_racine_delta:= s-s_racine**2;
+     p( 's-s_racine²',s_racine_delta);
+     is_square:= 0 = s_racine_delta;
 
-     P1:= Mean-Ecart;
-     P2:= Mean+Ecart;
+          if is_square then Traite_Carre
+     else                   Cas_General;
 
-     p('Blue_Circle_s', Blue_Circle_s);
-     p('Mean', Mean);
-     p('Ecart', Ecart);
-     p('P1',P1);
-     p('P2',P2);
+     l(lP1,P1);
+     l(lP2,P2);
+end;
 
-     FreeAndNil(Carre   );
-     FreeAndNil(Triangle);
+procedure TfQuasi_prime.bBatchClick(Sender: TObject);
+const
+     prime: array of integer
+     =
+      (
+      2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
+      );
+
+var
+   j: Integer;
+   n, nOK: Integer;
+   procedure T( _i1, _i2: Integer);
+   var
+      C: TCalcul;
+   begin
+        C:= Decompose( _i1 , _i2);
+        if C.Erreur
+        then
+            begin
+            m.Lines.Add(C.Log);
+            end
+        else
+            begin
+            //m.Lines.Add(C.Log);
+            Inc(nOK);
+            end;
+        Inc(n);
+   end;
+begin
+     nOK:= 0;
+     n:= 0;
+     for j:= Low(prime)+1 to High(prime)
+     do
+       begin
+       T(  prime[j-1],  prime[j]);
+       T(  prime[j],  prime[j]);
+       end;
+     m.Lines.Add(IntToStr(nOK)+' calculs ok sur '+IntToStr(n));
+
 end;
 
 end.
