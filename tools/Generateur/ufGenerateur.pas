a0a8c3c7ac4823e275571f3fd8a3dcac443fc6fe
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2022-08-05 15:17:19 +0200

    Déplacement des fichiers ufAutomatic* vers 06_Formes/Code_Generation

--- tools/Generateur/ufGenerateur.pas
+++ tools/Generateur/ufGenerateur.pas
@@ -13,14 +13,17 @@ type
 
  { TfGenerateur }
 
- TfGenerateur = class(TForm)
-  procedure FormCreate(Sender: TObject);
-  procedure FormShow(Sender: TObject);
- private
-  { private declarations }
- public
-  { public declarations }
- end;
+ TfGenerateur
+ =
+  class(TForm)
+   procedure FormCreate(Sender: TObject);
+   procedure FormShow(Sender: TObject);
+  private
+   { private declarations }
+   fAutomatic_VST: TfAutomatic_VST;
+  public
+   { public declarations }
+  end;
 
 var
  fGenerateur: TfGenerateur;
@@ -33,13 +36,21 @@ implementation
 
 procedure TfGenerateur.FormCreate(Sender: TObject);
 begin
-
+     fAutomatic_VST:= TfAutomatic_VST.Create( Self);
+     ClientWidth := fAutomatic_VST.Width;
+     ClientHeight:= fAutomatic_VST.Height;
+     fAutomatic_VST.Parent:= Self;
+     fAutomatic_VST.Align:= alClient;
+     //fAutomatic_VST.Menu:= nil;
+     //Menu:= fAutomatic_VST.mm;
+     //OnDropFiles:= @fAutomatic_VST.DropFiles;
+     //fjpFiles.Show;
 end;
 
 procedure TfGenerateur.FormShow(Sender: TObject);
 begin
      dmDatabase.Ouvre_db;
-     fAutomatic_VST.Show;
+     //fAutomatic_VST.Show;
 end;
 
 end.
