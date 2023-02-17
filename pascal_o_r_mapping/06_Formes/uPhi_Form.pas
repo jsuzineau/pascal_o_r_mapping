7df97dadcb644e140356986f2d3cca481b801b1a
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2023-02-17 15:54:03 +0100

    _

--- pascal_o_r_mapping/06_Formes/uPhi_Form.pas
+++ pascal_o_r_mapping/06_Formes/uPhi_Form.pas
@@ -1,39 +1,122 @@
 unit uPhi_Form;
 
-{$mode objfpc}{$H+}
+{$mode delphi}
 
 interface
 
 uses
     uPhi,
- Classes, SysUtils, Forms;
+ Classes, SysUtils, Forms, ExtCtrls;
 
 procedure Phi_Form_Up_horizontal  ( _F: TForm);
 procedure Phi_Form_Up_vertical    ( _F: TForm);
 procedure Phi_Form_Down_horizontal( _F: TForm);
 procedure Phi_Form_Down_vertical  ( _F: TForm);
 
+type
+
+ { ThPhi_Form }
+
+ ThPhi_Form
+ =
+  class
+  //Gestion  du cycle de vie
+  public
+    constructor Create( _F: TForm);
+    destructor Destroy; override;
+  //Attributs
+  private
+    F: TForm;
+    t: TTimer;
+    FormResize_running: Boolean;
+    procedure tTimer(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure Do_resize;
+  end;
+
 implementation
 
 procedure Phi_Form( _F: TForm; _Operation: TPhiSize_function);
 var
    P: TPoint;
-   Left, Top: Integer;
+   Left, Top, Width, Height: Integer;
+   dx, dy: Integer;
+   dLeft, dTop: Integer;
+   NewLeft  ,
+   NewTop   ,
+   NewWidth ,
+   NewHeight: Integer;
 begin
-     Left:= _F.Left;
-     Top := _F.Top ;
-     P.X:= _F.Width;
-     P.Y:= _F.Height;
+     Left  := _F.Left  ;
+     Top   := _F.Top   ;
+     Width := _F.Width ;
+     Height:= _F.Height;
 
-     P:= _Operation( P);
+     P:= _Operation( Point( Width, Height));
+     NewWidth := P.X;
+     NewHeight:= P.Y;
 
-     _F.SetBounds( Left, Top, P.X, P.Y);
+     dx:= NewWidth  - Width ;
+     dy:= NewHeight - Height;
+
+     {$IFDEF LINUX}
+       dLeft:= 0;
+       dTop:=  0;
+     {$ELSE}
+       dLeft:= 0;
+       dTop:=  0;
+     {$ENDIF}
+
+     NewLeft  := Left  + dLeft;
+     NewTop   := Top   + dTop;
+
+     _F.SetBounds( NewLeft, NewTop, NewWidth, NewHeight);
 end;
 
-procedure Phi_Form_Up_horizontal  ( _F: TForm);begin Phi_Form( _F, @PhiSizeUp_horizontal  ); end;
-procedure Phi_Form_Up_vertical    ( _F: TForm);begin Phi_Form( _F, @PhiSizeUp_vertical    ); end;
-procedure Phi_Form_Down_horizontal( _F: TForm);begin Phi_Form( _F, @PhiSizeDown_horizontal); end;
-procedure Phi_Form_Down_vertical  ( _F: TForm);begin Phi_Form( _F, @PhiSizeDown_vertical  ); end;
+procedure Phi_Form_Up_horizontal  ( _F: TForm);begin Phi_Form( _F, PhiSizeUp_horizontal  ); end;
+procedure Phi_Form_Up_vertical    ( _F: TForm);begin Phi_Form( _F, PhiSizeUp_vertical    ); end;
+procedure Phi_Form_Down_horizontal( _F: TForm);begin Phi_Form( _F, PhiSizeDown_horizontal); end;
+procedure Phi_Form_Down_vertical  ( _F: TForm);begin Phi_Form( _F, PhiSizeDown_vertical  ); end;
+
+constructor ThPhi_Form.Create( _F: TForm);
+begin
+     F:= _F;
+     F.onResize:= FormResize;
+
+     //t:= TTimer.Create( nil);
+     t.OnTimer:= tTimer;
+
+     FormResize_running:= False;
+end;
+
+destructor ThPhi_Form.Destroy;
+begin
+     //F.OnResize
+     FreeAndnil( t);
+end;
+
+procedure ThPhi_Form.FormResize(Sender: TObject);
+begin
+     if FormResize_running then exit;
+     try
+        //Do_resize;
+        t.Enabled:= False;
+        t.Enabled:= True;
+     finally
+            FormResize_running:= False;
+            end;
+end;
+
+procedure ThPhi_Form.tTimer(Sender: TObject);
+begin
+     t.Enabled:= False;
+     Do_resize;
+end;
+
+procedure ThPhi_Form.Do_resize;
+begin
+     Phi_Form_Up_horizontal( F);
+end;
 
 end.
 
--- tools/tests/jsPhi/ufPhi.pas
+++ tools/tests/jsPhi/ufPhi.pas
@@ -7,7 +7,7 @@ interface
 uses
     uPhi,
     uPhi_Form,
- Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+ Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
 
 type
 
@@ -15,16 +15,25 @@ type
 
  TfPhi = class(TForm)
   bDown_horizontal: TButton;
-  bUp_horizontal: TButton;
   bDown_vertical: TButton;
+  bLeft: TButton;
+  bUp_horizontal: TButton;
   bUp_vertical: TButton;
+  m: TMemo;
+  Panel1: TPanel;
+  t: TTimer;
   procedure bDown_horizontalClick(Sender: TObject);
+  procedure bLeftClick(Sender: TObject);
   procedure bUp_horizontalClick(Sender: TObject);
   procedure bDown_verticalClick(Sender: TObject);
   procedure bUp_verticalClick(Sender: TObject);
   procedure FormCreate(Sender: TObject);
   procedure FormResize(Sender: TObject);
+  procedure tTimer(Sender: TObject);
  private
+  FormResize_running: Boolean;
+  procedure Log_Top_Left( _S: String);
+  procedure Do_resize;
  public
 
  end;
@@ -40,12 +49,42 @@ implementation
 
 procedure TfPhi.FormCreate(Sender: TObject);
 begin
-
+     Log_Top_Left( 'FormCreate');
+     FormResize_running:= False;
 end;
 
 procedure TfPhi.FormResize(Sender: TObject);
 begin
+     if FormResize_running then exit;
+     try
+        //Do_resize;
+        t.Enabled:= False;
+        t.Enabled:= True;
+     finally
+            FormResize_running:= False;
+            end;
+end;
+
+procedure TfPhi.tTimer(Sender: TObject);
+begin
+     t.Enabled:= False;
+     Do_resize;
+end;
+
+procedure TfPhi.Do_resize;
+begin
+     Log_Top_Left( 'Do_resize, début');
      Phi_Form_Up_horizontal    ( Self);
+     Log_Top_Left( 'Do_resize, fin');
+end;
+
+procedure TfPhi.Log_Top_Left( _S: String);
+begin
+     m.Lines.Add(_S);
+     //m.Lines.Add('Left  : '+Format('%4d', [Left  ]));
+     m.Lines.Add('Top   : '+Format('%4d', [Top   ]));
+     //m.Lines.Add('Width : '+Format('%4d', [Width ]));
+     m.Lines.Add('Height: '+Format('%4d', [Height]));
 end;
 
 procedure TfPhi.bUp_horizontalClick(Sender: TObject);
@@ -56,6 +95,11 @@ end;
 procedure TfPhi.bDown_horizontalClick(Sender: TObject);
 begin
      Phi_Form_Down_horizontal  ( Self);
+end;
+
+procedure TfPhi.bLeftClick(Sender: TObject);
+begin
+     Left:= Left;
 end;
 
 procedure TfPhi.bUp_verticalClick(Sender: TObject);
