8da9267fc2074cea222eed2e8260decd2727ac77
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2025-09-13 00:30:32 +0200

    _

--- tools/jsBLE/Wio_Terminal/jsBLE/jsBLE.ino
+++ tools/jsBLE/Wio_Terminal/jsBLE/jsBLE.ino
@@ -1,61 +1,121 @@
-// source: https://wiki.seeedstudio.com/BLE-ibeacon-using-Wio-terminal/
-
-#include "sys/time.h"
 #include "rpcBLEDevice.h"
-#include "BLEBeacon.h"
-
-BLEAdvertising *pAdvertising;
-//struct timeval now;
-
-#define BEACON_UUID           "8ec76ea3-6668-48da-9866-75be8bc86f4d" // UUID 1 
-
-void setBeacon() {
-
-  BLEBeacon oBeacon = BLEBeacon();
-  oBeacon.setManufacturerId(0x4C00); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
-  oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
-  oBeacon.setMajor(0x007B);
-  oBeacon.setMinor(0x01C8);
-  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
-  BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
-  
-  oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04
-  
-  std::string strServiceData = "";
-  
-  strServiceData += (char)26;     // Len
-  strServiceData += (char)0xFF;   // Type
-  strServiceData += oBeacon.getData(); 
-  oAdvertisementData.addData(strServiceData);
-  
-  pAdvertising->setAdvertisementData(oAdvertisementData);
-  pAdvertising->setScanResponseData(oScanResponseData);
-  pAdvertising->setAdvertisementType(GAP_ADTYPE_ADV_NONCONN_IND);
-
-}
-
+#include <BLE2902.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+TFT_eSprite spr = TFT_eSprite(&tft);  // Sprite 
+ 
+BLEServer *pServer = NULL;
+BLECharacteristic * pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+String Value11;
+ 
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+ 
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      spr.fillSprite(TFT_BLACK);
+      spr.createSprite(240, 100);
+      spr.setTextColor(TFT_WHITE, TFT_BLACK);
+      spr.setFreeFont(&FreeSansBoldOblique12pt7b);
+      spr.drawString("Message: ", 20, 70);
+      spr.setTextColor(TFT_GREEN, TFT_BLACK);
+      spr.drawString("status: connected",10 ,5); 
+      spr.pushSprite(0, 0);
+    };
+ 
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      Serial.print("123123");
+      spr.fillSprite(TFT_BLACK);
+      spr.createSprite(240, 100);
+      spr.setTextColor(TFT_WHITE, TFT_BLACK);
+      spr.setFreeFont(&FreeSansBoldOblique12pt7b);
+      spr.drawString("Message: ", 20, 70);
+      spr.setTextColor(TFT_RED, TFT_BLACK);
+      spr.drawString("status: disconnect",10 ,5); 
+      spr.pushSprite(0, 0);
+    }
+};
+ 
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+ 
+        if (rxValue.length() > 0) {
+        spr.fillSprite(TFT_BLACK);
+        spr.setTextColor(TFT_WHITE, TFT_BLACK);
+        spr.setFreeFont(&FreeSansBoldOblique9pt7b);
+        for (int i = 0; i < rxValue.length(); i++){
+//           Serial.print(rxValue[i]);
+           spr.drawString((String)rxValue[i],10 + i*15,0);
+        spr.pushSprite(10, 100);
+        }
+       }
+    }
+};
+ 
 void setup() {
-
-    
-  Serial.begin(115200);
-  while(!Serial){};
-  
-  // Create the BLE Device
-  BLEDevice::init("");
-
+  tft.begin();
+  tft.init();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_BLACK);
+ 
+  BLEDevice::init("UART Servicess");  //device name define
+ 
   // Create the BLE Server
-  // BLEServer *pServer = BLEDevice::createServer(); // <-- no longer required to instantiate BLEServer, less flash and ram usage
-
-  pAdvertising = BLEDevice::getAdvertising();
-  
-  setBeacon();
-   // Start advertising
-  pAdvertising->start();
-  Serial.println("Advertizing started...");
-  delay(100);
-  Serial.printf("in deep sleep\n");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+ 
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+ 
+  // Create a BLE Characteristic
+  pTxCharacteristic = pService->createCharacteristic(
+                    CHARACTERISTIC_UUID_TX,
+                    BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ
+                  );
+  pTxCharacteristic->setAccessPermissions(GATT_PERM_READ);      
+  pTxCharacteristic->addDescriptor(new BLE2902());
+ 
+  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
+                       CHARACTERISTIC_UUID_RX,
+                      BLECharacteristic::PROPERTY_WRITE
+ 
+                    );
+  pRxCharacteristic->setAccessPermissions(GATT_PERM_READ | GATT_PERM_WRITE);           
+ 
+  pRxCharacteristic->setCallbacks(new MyCallbacks());
+ 
+  // Start the service
+  pService->start();
+ 
+  // Start advertising
+  pServer->getAdvertising()->start();
+      spr.fillSprite(TFT_BLACK);
+      spr.createSprite(240, 100);
+      spr.setTextColor(TFT_WHITE, TFT_BLACK);
+      spr.setFreeFont(&FreeSansBoldOblique12pt7b);
+      spr.drawString("status: disconnect",10 ,5); 
+      spr.drawString("Message: ", 20, 70);
+      spr.pushSprite(0, 0);
 }
-
+ 
 void loop() {
- delay(1000);
+ 
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
