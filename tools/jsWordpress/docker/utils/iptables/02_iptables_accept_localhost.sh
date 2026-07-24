a4eaab4c703834c6ad9e6f4910de5906a87e7e92
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2026-07-24 23:13:09 +0200

    _

--- tools/jsWordpress/docker/utils/iptables/02_iptables_accept_localhost.sh
+++ tools/jsWordpress/docker/utils/iptables/02_iptables_accept_localhost.sh
@@ -1,1 +1,2 @@
 sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
--- tools/jsWordpress/docker/utils/iptables/template_04_iptables_accept_local_DNS.sh
+++ tools/jsWordpress/docker/utils/iptables/template_04_iptables_accept_local_DNS.sh
@@ -1,3 +1,2 @@
-export interface_externe=(your outside interface here)
-sudo iptables -A INPUT ! -i $interface_externe -p tcp --dport 53 -j ACCEPT
-sudo iptables -A INPUT ! -i $interface_externe -p udp --dport 53 -j ACCEPT
+sudo iptables -A OUTPUT ! -p udp --dport 53 -j ACCEPT
+sudo iptables -A OUTPUT ! -p tcp --dport 53 -j ACCEPT
