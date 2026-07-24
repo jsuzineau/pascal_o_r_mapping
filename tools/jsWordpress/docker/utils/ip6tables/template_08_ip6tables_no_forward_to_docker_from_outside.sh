c72c8bcf0f7130581603aea76896c8f63207c622
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2026-07-25 01:09:43 +0200

    _

--- tools/jsWordpress/docker/utils/ip6tables/template_08_ip6tables_no_forward_to_docker_from_outside.sh
+++ tools/jsWordpress/docker/utils/ip6tables/template_08_ip6tables_no_forward_to_docker_from_outside.sh
@@ -1,1 +1,3 @@
-sudo ip6tables -I DOCKER-USER -i (your outside interface here)  -j DROP
+export interface_externe=(your outside interface here) 
+sudo ip6tables -A DOCKER-USER -i $interface_externe -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo ip6tables -A DOCKER-USER -i $interface_externe -j DROP
