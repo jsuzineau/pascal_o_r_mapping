b7be9d0f50994f2702a621805c127249d589c5ab
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2026-07-24 23:19:05 +0200

    _

--- tools/jsWordpress/docker/utils/iptables/04_iptables_accept_output_DNS.sh
+++ tools/jsWordpress/docker/utils/iptables/04_iptables_accept_output_DNS.sh
@@ -1,2 +1,2 @@
-sudo iptables -A OUTPUT ! -p udp --dport 53 -j ACCEPT
-sudo iptables -A OUTPUT ! -p tcp --dport 53 -j ACCEPT
+sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
