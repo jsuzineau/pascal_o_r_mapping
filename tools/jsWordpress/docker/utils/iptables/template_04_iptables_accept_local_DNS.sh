27e382c07d511b1f723c2650ff29385669d7029d
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2026-07-24 22:42:00 +0200

    _

--- tools/jsWordpress/docker/utils/iptables/template_04_iptables_accept_local_DNS.sh
+++ tools/jsWordpress/docker/utils/iptables/template_04_iptables_accept_local_DNS.sh
@@ -1,1 +1,2 @@
-sudo iptables -A INPUT ! -i (your outside interface here) --dport 53 -j ACCEPT
+sudo iptables -A INPUT ! -i (your outside interface here) tcp --dport 53 -j ACCEPT
+sudo iptables -A INPUT ! -i (your outside interface here) udp --dport 53 -j ACCEPT
